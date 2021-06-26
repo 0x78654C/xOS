185b3b0156d76a7ff5d59fb508490e96a655d292
0x78654C <l2xguard@live.com>
2021-06-26 21:50:59 +0300

    Users management
    work in progress

---
+++ xOS/FileSystem/Users.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xOS.FileSystem
+{
+    public class Users
+    {
+        Crypto.MEX mex = new Crypto.MEX("theX0SSYstemOp3RRa0R!");
+        private string UsrFile;
+        private int index = 0;
+        
+        
+        public string E_PWD(string data)
+        {
+            return mex.Encrypt(data);
+        }
+
+        public string D_PWD(string data)
+        {
+            return mex.Decrypt(data); 
+        }
+
+
+
+    }
+}
