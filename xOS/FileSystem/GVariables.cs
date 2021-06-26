c8bec312a8a01f14b4ac47684fcc87b0d14897bc
0x78654C <l2xguard@live.com>
2021-06-26 21:50:14 +0300

    System struct variable

---
+++ xOS/FileSystem/GVariables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xOS.FileSystem
+{
+    public static class GVariables
+    {
+        /*Decalre Global Variables*/
+        public static string SysVol = "0:\\";
+        public static string SysDir = SysVol+"Sys";
+        public static string UsrDir = SysDir+"\\Usr";
+        public static string LogDir = SysDir+"\\Log";
+        public static string SYSLogFile = LogDir+ "\\logSYS.l";
+        public static string UsrFile = UsrDir+ "\\usr.u";
+    }
+}
