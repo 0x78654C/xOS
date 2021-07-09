47efc7691e4ab48f6fb1379dbd55035e7fea4338
0x78654C <l2xguard@live.com>
2021-07-09 23:56:23 +0300

    Changed
    Encryption function call

---
+++ xOS/UserManagement/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xOS.FileSystem
+{
+    public static class Users
+    {
+       
+        private static string UsrFile;
+        private static  int index = 0;
+        
+        
+        public static string E_PWD(string data)
+        {
+            return Cryptography.Encrypt(data,"TestPass@!#!!@#12");
+        }
+
+        public static string D_PWD(string data)
+        {
+            return Cryptography.Decrypt(data, "TestPass@!#!!@#12");
+        }
+
+    }
+}
