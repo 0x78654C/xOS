914d13d1c2a4f73e03e9a45972159698d3cb0341
0x78654C <l2xguard@live.com>
2021-07-10 22:37:01 +0300

    - added UserLogin method from Root.cs - added First user initialization on first OS run - added hide password on type method for user login  and user initialization on first run

--- xOS/UserManagement/Users.cs
+++ xOS/UserManagement/Users.cs
@@ -1,25 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace xOS.FileSystem
 {
     public static class Users
     {
-       
-        private static string UsrFile;
-        private static  int index = 0;
-        
-        
+
+        private static string UsrFile = GVariables.UsrFile;
+        private static int index = 0;
+
+
+        //login system
+        public static string UserLogin()
+        {
+
+            Console.WriteLine("--Login--");
+            Console.Write("User Name: ");
+            string user = Console.ReadLine();
+            string o = string.Empty;
+            try
+            {
+                string[] UserFileRead;
+                if (System.IO.File.Exists(UsrFile))
+                {
+                    UserFileRead = System.IO.File.ReadAllLines(UsrFile);
+                    foreach (var User in UserFileRead)
+                    {
+                        if (User.Split('|')[0] == user)
+                        {
+                            string UserP = User.Split('|')[1];
+                            Console.Write("User Password: ");
+                            string pass = GetHiddenConsoleInput();
+
+                            if (pass != UserP)
+                            {
+                                Console.WriteLine("Wrong passwsord!");
+                                o = "Wrong passwsord!";
+                            }
+                            else
+                            {
+                                o = $"logged|{User}";
+                                CLog.CLog.SysLog_LoadOS($"User: {user} logged");
+                                Console.Clear();
+                            }
+                        }
+                        else
+                        {
+                            o = $"User {user} dose not exist!";
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            if (o.Contains("dose not exist"))
+            {
+                Console.WriteLine($"User {user} dose not exist!");
+            }
+            return o;
+        }
+
+        public static void Initilize_First_User()
+        {
+    
+            try
+            {
+                Console.WriteLine("Welcome to xOS. To use this operating system you need to create first a local account. \n");
+                Console.Write("User Name: ");
+                string UserName = Console.ReadLine();
+                Console.Write("User Password: ");
+                string UserPass = GetHiddenConsoleInput();
+                string UsrFileRead;
+
+                //we check if user file exists
+                if (System.IO.File.Exists(UsrFile))
+                {
+                    UsrFileRead = System.IO.File.ReadAllText(UsrFile);
+
+                    //we check if user exists in file
+                    if (UsrFileRead.Contains(UserName))
+                    {
+                        Console.WriteLine($"User {UserName}, already exist!");
+                    }
+                    else
+                    {
+                        System.IO.File.AppendAllText(UsrFile, $"{UserName}|{UserPass}\n");
+                        Console.WriteLine($"Created user: {UserName}");
+                        CLog.CLog.SysLog_LoadOS($"Created user: {UserName}");
+                    }
+                }
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+
+        }
+        //---------------------------------------------
         public static string E_PWD(string data)
         {
-            return Cryptography.Encrypt(data,"TestPass@!#!!@#12");
+            return Cryptography.Encrypt(data, "TestPass@!#!!@#12");
         }
 
         public static string D_PWD(string data)
         {
             return Cryptography.Decrypt(data, "TestPass@!#!!@#12");
         }
-
+        /// <summary>
+        /// Hidding password imput for strings
+        /// </summary>
+        /// <returns></returns>
+        private static string GetHiddenConsoleInput()
+        {
+            StringBuilder input = new StringBuilder();
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter) break;
+                if (key.Key == ConsoleKey.Backspace && input.Length > 0) input.Remove(input.Length - 1, 1);
+                else if (key.Key != ConsoleKey.Backspace) input.Append(key.KeyChar);
+            }
+            return input.ToString();
+        }
     }
 }
