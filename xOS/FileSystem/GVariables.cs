615f493ce9f69633fd3b5454024106bd935e71c7
0x78654C <l2xguard@live.com>
2021-07-16 23:01:18 +0300

    - new dirs and files created: Tmp, cDir.t (store current directory location), login.t (store login state)

--- xOS/FileSystem/GVariables.cs
+++ xOS/FileSystem/GVariables.cs
@@ -11,6 +11,9 @@ namespace xOS.FileSystem
         public static string SysDir = SysVol+"Sys";
         public static string UsrDir = SysDir+"\\Usr";
         public static string LogDir = SysDir+"\\Log";
+        public static string TmpDir = SysDir + "\\Tmp";
+        public static string cDirFile = TmpDir + "\\cDir.t";
+        public static string LoginFile = TmpDir + "\\login.t";
         public static string SYSLogFile = LogDir+ "\\logSYS.l";
         public static string UsrFile = UsrDir+ "\\usr.u";
     }
