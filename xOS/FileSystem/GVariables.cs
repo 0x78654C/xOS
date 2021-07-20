e7084ba8fdbf3443449efe3e93f0690e38eae39d
0x78654C <l2xguard@live.com>
2021-07-20 23:36:11 +0300

    added variable for Users directory . work in progress

--- xOS/FileSystem/GVariables.cs
+++ xOS/FileSystem/GVariables.cs
@@ -9,6 +9,7 @@ namespace xOS.FileSystem
         /*Decalre Global Variables*/
         public static string SysVol = "0:\\";
         public static string SysDir = SysVol+"Sys";
+        public static string UsersDir = SysVol + "Users";
         public static string UsrDir = SysDir+"\\Usr";
         public static string LogDir = SysDir+"\\Log";
         public static string TmpDir = SysDir + "\\Tmp";
