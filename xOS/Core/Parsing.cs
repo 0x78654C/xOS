40a34a41c3989f5b83fd12faf61c53902c5a78aa
0x78654C <l2xguard@live.com>
2021-07-27 16:26:19 +0300

    Added parsing class for future use. Added in it the parsing folder for copy/move file command

---
+++ xOS/Core/Parsing.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using xOS.FileSystem;
+
+namespace xOS.Core
+{
+    public static class Parsing
+    {
+        private static string cDirFile = GVariables.cDirFile; //current directory location
+        /// <summary>
+        /// Get the path of file or adding the current directory path
+        /// Used for file copy/move methods
+        /// </summary>
+        /// <param name="FilePath"></param>
+        /// <returns></returns>
+        public static string ParseFilePath(string FilePath)
+        {
+            string path;
+            string lastSplit;
+            string cDir;
+            if (FilePath.Contains("\\"))
+            {
+                int c = 0;
+
+                foreach (char delimiter in FilePath)
+                {
+                    if (delimiter == '\\')
+                    {
+                        c++;
+                    }
+                }
+                lastSplit = FilePath.Split('\\')[c];
+                FilePath = FilePath.Replace(lastSplit, "");
+                path = FilePath;
+                return path;
+            }
+            else
+            {
+                cDir = File.ReadAllText(cDirFile);
+                path = cDir;
+            }
+            return path;
+        }
+    }
+}
