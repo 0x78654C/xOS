0469ca9703ee9a030d54071d2abf7eb5201e2bce
0x78654C <l2xguard@live.com>
2021-08-03 22:32:02 +0300

    - refactoring: renaming , removing redundant code, simplify code - fixed check current directory bug

--- xOS/FileSystem/FileManagement.cs
+++ xOS/FileSystem/FileManagement.cs
@@ -23,16 +23,9 @@ namespace xOS.FileSystem
             {
                 string cDir = File.ReadAllText(s_CurrentDirectory);
                 fileName = fileName.Split(' ')[1];
-                if (string.IsNullOrEmpty(cDir) && fileName.Contains(@"0:\"))
-                {
-                    Sys.FileSystem.VFS.VFSManager.CreateFile(fileName);
-                    Console.WriteLine($"File {fileName} was created!");
-                }
-                else
-                {
-                    Sys.FileSystem.VFS.VFSManager.CreateFile(cDir + @"\" + fileName);
-                    Console.WriteLine($"File {fileName} was created!");
-                }
+                fileName = !string.IsNullOrEmpty(cDir) && fileName.Contains(@"0:\") ? fileName : cDir + @"\" + fileName;
+                Sys.FileSystem.VFS.VFSManager.CreateFile(fileName);
+                Console.WriteLine($"File {fileName} was created!");
             }
             catch (Exception e)
             {
@@ -48,18 +41,13 @@ namespace xOS.FileSystem
         {
             try
             {
+                string trueMessage = $"File {fileName} was deleted!";
+                string falseMessage = $"File {fileName} was not deleted!";
                 fileName = fileName.Split(' ')[1];
                 string cDir = File.ReadAllText(s_CurrentDirectory);
-                if (string.IsNullOrEmpty(cDir) && fileName.Contains(@"0:\"))
-                {
-                    File.Delete(fileName);
-                    Console.WriteLine($"File {fileName} was deleted!");
-                }
-                else
-                {
-                    File.Delete(cDir + @"\" + fileName);
-                    Console.WriteLine($"File {fileName} was deleted!");
-                }
+                fileName = !string.IsNullOrEmpty(cDir) && fileName.Contains(@"0:\") ? fileName : cDir + @"\" + fileName;
+                File.Delete(fileName);
+                CheckFile(fileName, trueMessage, falseMessage);
             }
             catch (Exception e)
             {
@@ -78,43 +66,26 @@ namespace xOS.FileSystem
             {
                 fileName = fileName.Split(' ')[1];
                 string cDir = File.ReadAllText(s_CurrentDirectory);
-                if (string.IsNullOrEmpty(cDir) && fileName.Contains(@"0:\"))
-                {
-                    if (File.Exists(fileName))
-                    {
-                        var hello_file = Sys.FileSystem.VFS.VFSManager.GetFile(fileName);
-                        var hello_file_stream = hello_file.GetFileStream();
+                fileName = !string.IsNullOrEmpty(cDir) && fileName.Contains(@"0:\") ? fileName : cDir + @"\" + fileName;
 
-                        if (hello_file_stream.CanRead)
-                        {
-                            byte[] text_to_read = File.ReadAllBytes(fileName);
-                            Console.WriteLine(Encoding.UTF8.GetString(text_to_read));
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"FIle {fileName} does not exit!");
-                    }
-                }
-                else
+                if (File.Exists(fileName))
                 {
-                    fileName = cDir + @"\" + fileName;
-                    if (File.Exists(fileName))
-                    {
-                        var hello_file = Sys.FileSystem.VFS.VFSManager.GetFile(fileName);
-                        var hello_file_stream = hello_file.GetFileStream();
+                    var hello_file = Sys.FileSystem.VFS.VFSManager.GetFile(fileName);
+                    var hello_file_stream = hello_file.GetFileStream();
 
-                        if (hello_file_stream.CanRead)
-                        {
-                            byte[] text_to_read = File.ReadAllBytes(fileName);
-                            Console.WriteLine(Encoding.UTF8.GetString(text_to_read));
-                        }
-                    }
-                    else
+                    if (hello_file_stream.CanRead)
                     {
-                        Console.WriteLine($"FIle {fileName} does not exit!");
+                        byte[] text_to_read = File.ReadAllBytes(fileName);
+                        Console.WriteLine(Encoding.UTF8.GetString(text_to_read));
+                        return;
                     }
+
+                    Console.WriteLine($"File {fileName} cannot be read!");
+                    return;
                 }
+
+                Console.WriteLine($"File {fileName} does not exit!");
+
             }
             catch (Exception e)
             {
@@ -133,53 +104,16 @@ namespace xOS.FileSystem
             {
                 string Fn = fileName.Split(' ')[1];
                 string cDir = File.ReadAllText(s_CurrentDirectory);
-                if (string.IsNullOrEmpty(cDir) && fileName.Contains(@"0:\"))
-                {
-                    Console.Write("Type: ");
-                    string Data = Console.ReadLine();
-                    Console.WriteLine(Data);
-                    var hello_file = Sys.FileSystem.VFS.VFSManager.GetFile(@Fn);
-                    if (File.Exists(@Fn))
-                    {
-                        var hello_file_stream = hello_file.GetFileStream();
-
-                        if (hello_file_stream.CanWrite)
-                        {
-                            File.WriteAllText(@Fn, Data);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {fileName} does not exist!");
-                    }
-                }
-                else
-                {
-                    Fn = cDir + @"\" + Fn;
-                    Console.Write("Type: ");
-                    string Data = Console.ReadLine();
-                    Console.WriteLine(Data);
-                    var hello_file = Sys.FileSystem.VFS.VFSManager.GetFile(@Fn);
-                    if (File.Exists(@Fn))
-                    {
-                        var hello_file_stream = hello_file.GetFileStream();
-
-                        if (hello_file_stream.CanWrite)
-                        {
-                            File.WriteAllText(@Fn, Data);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {fileName} does not exist!");
-                    }
-                }
+                @Fn = !string.IsNullOrEmpty(cDir) && fileName.Contains(@"0:\") ? @Fn : cDir + @"\" + @Fn;
+                Write(@Fn, false);  
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
+                Console.WriteLine(e.Message);
             }
         }
+
+
 
         /// <summary>
         /// Appends data to a file. Command: af
@@ -190,51 +124,18 @@ namespace xOS.FileSystem
             try
             {
                 string Fn = fileName.Split(' ')[1];
-                string Data = fileName.Split(' ')[2];
                 string cDir = File.ReadAllText(s_CurrentDirectory);
-                if (string.IsNullOrEmpty(cDir) && fileName.Contains(@"0:\"))
-                {
-                    var hello_file = Sys.FileSystem.VFS.VFSManager.GetFile(@Fn);
-                    if (File.Exists(@Fn))
-                    {
-                        var hello_file_stream = hello_file.GetFileStream();
-                        var read_file = File.ReadAllText(@Fn);
-
-                        if (hello_file_stream.CanWrite)
-                        {
-                            File.WriteAllText(@Fn, read_file + Data);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {Fn} does not exist!");
-                    }
-                }
-                else
-                {
-                    Fn = cDir + @"\" + Fn;
-                    var hello_file = Sys.FileSystem.VFS.VFSManager.GetFile(@Fn);
-                    if (File.Exists(@Fn))
-                    {
-                        var hello_file_stream = hello_file.GetFileStream();
-                        var read_file = File.ReadAllText(@Fn);
-
-                        if (hello_file_stream.CanWrite)
-                        {
-                            File.WriteAllText(@Fn, read_file + Data);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {Fn} does not exist!");
-                    }
-                }
+                @Fn = !string.IsNullOrEmpty(cDir) && fileName.Contains(@"0:\") ? @Fn : cDir + @"\" + @Fn;
+                Write(@Fn, true);
+                return;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
         }
+
+
 
         /// <summary>
         /// Copy file to a certain path. Command: fcopy
@@ -252,56 +153,23 @@ namespace xOS.FileSystem
 
                 if ((sPath != cDir) && (dPath != cDir)) //path of destination and source file is not equal to current dir
                 {
-                    if (!File.Exists(destination) && File.Exists(source))
-                    {
-                        File.Copy(source, destination);
-                        Console.WriteLine($"File {source} copied to {destination}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {destination} already exists or source file does not exist!");
-                    }
+                    Copy(source, destination, false);
                 }
                 else if ((sPath == cDir) && (dPath != cDir)) //path of source equals the current directory but destination does not
                 {
                     source = cDir + @"\" + source;
-
-                    if (!File.Exists(destination) && File.Exists(source))
-                    {
-                        File.Copy(source, destination);
-                        Console.WriteLine($"File {source} copied to {destination}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {destination} already exists or source file does not exist!");
-                    }
+                    Copy(source, destination, false);
                 }
                 else if ((sPath != cDir) && (dPath == cDir)) //path of destination equals the current directory but source does not
                 {
                     destination = cDir + @"\" + destination;
-                    if (!File.Exists(destination) && File.Exists(source))
-                    {
-                        File.Copy(source, destination);
-                        Console.WriteLine($"File {source} copied to {destination}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {destination} already exists or source file does not exist!");
-                    }
+                    Copy(source, destination, false);
                 }
                 else if ((sPath == cDir) && (dPath == cDir)) //both source and destination equals to current path
                 {
                     source = cDir + @"\" + source;
                     destination = cDir + @"\" + destination;
-                    if (!File.Exists(destination) && File.Exists(source))
-                    {
-                        File.Copy(source, destination);
-                        Console.WriteLine($"File {source} copied to {destination}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {destination} already exists or source file does not exist!");
-                    }
+                    Copy(source, destination, false);
                 }
             }
             catch (Exception e)
@@ -326,92 +194,97 @@ namespace xOS.FileSystem
 
                 if ((sPath != cDir) && (dPath != cDir)) //path of destination and source file is not equal to current dir
                 {
-                    if (!File.Exists(destination) && File.Exists(source))
-                    {
-                        File.Copy(source, destination);
-                        if (File.Exists(destination))
-                        {
-                            File.Delete(source);
-                            Console.WriteLine($"File {source} moved to {destination}");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"File {source} could not be moved!");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {destination} already exists or source file does not exist!");
-                    }
+                    Copy(source, destination, true);
                 }
                 else if ((sPath == cDir) && (dPath != cDir)) //path of source equals the current directory but destination does not
                 {
                     source = cDir + @"\" + source;
-                    if (!File.Exists(destination) && File.Exists(source))
-                    {
-                        File.Copy(source, destination);
-                        if (File.Exists(destination))
-                        {
-                            File.Delete(source);
-                            Console.WriteLine($"File {source} moved to {destination}");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"File {source} could not be moved!");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {destination} already exists or source file does not exist!");
-                    }
+                    Copy(source, destination, true);
                 }
                 else if ((sPath != cDir) && (dPath == cDir)) //path of destination equals the current directory but source does not
                 {
                     destination = cDir + @"\" + destination;
-                    if (!File.Exists(destination) && File.Exists(source))
-                    {
-                        File.Copy(source, destination);
-                        if (File.Exists(destination))
-                        {
-                            File.Delete(source);
-                            Console.WriteLine($"File {source} moved to {destination}");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"File {source} could not be moved!");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {destination} already exists or source file does not exist!");
-                    }
+                    Copy(source, destination, true);
                 }
                 else if ((sPath == cDir) && (dPath == cDir)) //both source and destination equals to current path
                 {
                     source = cDir + @"\" + source;
                     destination = cDir + @"\" + destination;
-                    if (!File.Exists(destination) && File.Exists(source))
-                    {
-                        File.Copy(source, destination);
-                        if (File.Exists(destination))
-                        {
-                            File.Delete(source);
-                            Console.WriteLine($"File {source} moved to {destination}");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"File {source} could not be moved!");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"File {destination} already exists or source file does not exist!");
-                    }
+                    Copy(source, destination, true);
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+            }
+        }
+
+        // File check if exists
+        private static void CheckFile(string fileName, string msgTrue, string msgFalse)
+        {
+            if (File.Exists(fileName))
+            {
+                Console.WriteLine(msgTrue);
+            }
+            else
+            {
+                Console.WriteLine(msgFalse);
+            }
+        }
+
+
+        // Copy/Move
+        private static void Copy(string source, string destination, bool move)
+        {
+            if (!File.Exists(destination) && File.Exists(source))
+            {
+                if (!move)
+                {
+                    File.Copy(source, destination);
+                    Console.WriteLine($"File {source} copied to {destination}");
+                    return;
+                }
+                File.Copy(source, destination);
+                if (!File.Exists(destination))
+                {
+                    Console.WriteLine($"File {source} could not be moved!");
+                    return;
+                }
+
+                File.Delete(source);
+                Console.WriteLine($"File {source} moved to {destination}");
+                return;
+            }
+            Console.WriteLine($"File {destination} already exists or source file does not exist!");
+            return;
+        }
+
+        // Write/Append file 
+        private static void Write(string fileName, bool append)
+        {
+            Console.Write("Type: ");
+            string Data = Console.ReadLine();
+            byte[] dataBytes = Encoding.ASCII.GetBytes(Data);
+            Console.WriteLine(Data);
+            var hello_file = Sys.FileSystem.VFS.VFSManager.GetFile(fileName);
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine($"File {fileName} does not exist!");
+                return;
+            }
+
+            var hello_file_stream = hello_file.GetFileStream();
+
+            if (hello_file_stream.CanWrite)
+            {
+                if (!append)
+                {
+                    hello_file_stream.Write(dataBytes, 0, dataBytes.Length);
+                    return;
+                }
+                var read_file = File.ReadAllText(fileName);
+                File.WriteAllText(fileName, read_file + Data);
+                return;
             }
         }
     }
