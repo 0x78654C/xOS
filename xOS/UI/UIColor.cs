c1d280ef0aa31984bab72008103c4e573e2bedf1
xeL <xcoding.dev@gmail.com>
2024-08-09 23:51:20 +0300

    created class for colorize UI

---
+++ xOS/UI/UIColor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace xOS.UI
+{
+    public class UIColor
+    {
+        /// <summary>
+        /// Change color of a specific line in console. 
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="text"></param>
+        public static void ColorConsoleTextLine(ConsoleColor color, string text)
+        {
+            ConsoleColor currentForegrond = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = currentForegrond;
+        }
+
+
+        /// <summary>
+        /// Change color of a specific text in console. 
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="text"></param>
+        public static void ColorConsoleText(ConsoleColor color, string text)
+        {
+            ConsoleColor currentForegrond = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = currentForegrond;
+        }
+
+
+        /// <summary>
+        /// Display error message in red. 
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="text"></param>
+        public static void ErrorConsoleTextLine(string text)
+        {
+            ConsoleColor currentForegrond = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"Error: {text}");
+            Console.ForegroundColor = currentForegrond;
+        }
+    }
+}
