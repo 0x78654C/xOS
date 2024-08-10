be10136a739f8bfbf6531173c24d9cea55da3942
xeL <xcoding.dev@gmail.com>
2024-08-11 00:00:24 +0300

    added method for get directory size. WIP for sped

--- xOS/FileSystem/Conversion.cs
+++ xOS/FileSystem/Conversion.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
 using System.IO;
 
 namespace xOS.FileSystem
@@ -63,6 +65,41 @@ namespace xOS.FileSystem
                 }
                 return String.Format("{0:0.##} {1}", len, s_sizes[order]);
             }
+        }
+
+        /// <summary>
+        /// Get directory size in bytes.
+        /// </summary>
+        /// <param name="directoryInfo"></param>
+        /// <returns></returns>
+        private static double DirSize(DirectoryInfo directoryInfo)
+        {
+            double size = 0;
+            var fileInfos = directoryInfo.GetFiles();
+            foreach (var fileInfo in fileInfos)
+                size += fileInfo.Length;
+
+            var directoryInfos = directoryInfo.GetDirectories();
+            foreach (var dirInfo in directoryInfos)
+                size += DirSize(dirInfo);
+            return size;
+        }
+
+        /// <summary>
+        /// Get directory size.
+        /// </summary>
+        /// <param name="directoryInfo"></param>
+        /// <returns></returns>
+        public static string GetDirSize(DirectoryInfo directoryInfo)
+        {
+            var order = 0;
+            double length = DirSize(directoryInfo);
+            while(length >= 1024 && order < s_sizes.Length - 1)
+            {
+                order++;
+                length /= 1024;
+            }
+            return String.Format("{0:0.##} {1}", length, s_sizes[order]);
         }
     }
 }
