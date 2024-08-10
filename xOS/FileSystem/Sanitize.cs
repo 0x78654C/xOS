19e8b6cd86f9d57a0c8362a59236bffd5e9d051e
xeL <xcoding.dev@gmail.com>
2024-08-11 00:01:05 +0300

    added method for sanitize path if current dir is included

---
+++ xOS/FileSystem/Sanitize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xOS.FileSystem
+{
+    public class Sanitize
+    {
+        /// <summary>
+        /// Sanitize path.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="currentDir"></param>
+        /// <returns></returns>
+        public static string SanitizePath(string path, string currentDir) => path.Contains(":") && path.Contains("\\") ? path : $@"{currentDir}{path}";
+    }
+}
